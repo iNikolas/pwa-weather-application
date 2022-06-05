ac9a044220d0b4f8015250b8b8aebe8934bdb1c3
Mykola Lebid <realkolos1@gmail.com>
2022-06-05 19:08:42 +0300

    Updates

--- serviceworker.js
+++ serviceworker.js
@@ -1,28 +1,50 @@
-const CACHE_NAME = "version-2";
+const STATIC_CACHE_NAME = "s-version-1";
+const DYNAMIC_CACHE_NAME = "d-version-1";
 const urlsToCache = ["index.html", "offline.html"];
 
 const self = this;
 
 self.addEventListener("install", async (event) => {
-  const cache = await caches.open(CACHE_NAME);
+  const cache = await caches.open(STATIC_CACHE_NAME);
   await cache.addAll(urlsToCache);
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then(() =>
-        fetch(event.request).catch(() => caches.match("offline.html"))
-      )
-  );
+  const { request } = event;
+  const url = new URL(request.url);
+
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(request));
+  } else {
+    event.respondWith(networkFirst(request));
+  }
 });
 
 self.addEventListener("activate", async (event) => {
   const cacheNames = await caches.keys();
   await Promise.all(
     cacheNames
-      .filter((name) => name !== CACHE_NAME)
+      .filter((name) => name !== STATIC_CACHE_NAME)
+      .filter((name) => name !== DYNAMIC_CACHE_NAME)
       .map((name) => caches.delete(name))
   );
 });
+
+async function cacheFirst(request) {
+  const cached = await caches.match(request);
+  return cached ?? (await fetch(request));
+}
+
+async function networkFirst(request) {
+  const cache = await caches.open(DYNAMIC_CACHE_NAME);
+  try {
+    const response = await fetch(request);
+    await cache.put(request, response.clone());
+
+    return response;
+  } catch (e) {
+    const cached = cache.match(request);
+
+    return cached ?? (await caches.match("offline.html"));
+  }
+}
